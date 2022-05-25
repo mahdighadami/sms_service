badb41a1fce8cc7236a6c5cdc0654b7153be17f8
Mahdi <mahdighadami2@gmail.com>
2022-05-26 00:17:17 +0430

    edit send sms function. use cURL functions.

--- sms.php
+++ sms.php
@@ -4,21 +4,26 @@ function readphones($filename){
     return explode("\n", file_get_contents($filename));
 }
 
-
-
 function send_sms($number, $text){
-    //TODO: sen sms
-    $data = array();
-    $url = 'https://example.come/...';
+    //TODO: send sms
+    $data = array(
+        'receptor' => $number,
+        'message' => $text
+    );
     $api_key = '2424for424example3434';
+    $url = 'https://example.come/' . $api_key . '.../';
+
+    $curl_sess = curl_init();
+    curl_setopt($curl_sess, CURLOPT_URL, $url);
+    curl_setopt($curl_sess, CURLOPT_POST, 1);
+    curl_setopt($curl_sess, CURLOPT_POSTFIELDS, $data);
 }
 
 $phones = readphones("phones.txt");
+$text = 'test sms message';
 //var_dump($phones);
 $text = "Hello, World";
 foreach($phones as $phone){
-    $phone = trim($phone);
-    send_sms($phone, $text);
+    //send_sms($phone, $text);
     echo $phone;?> <br> <?php ;
 }
-
